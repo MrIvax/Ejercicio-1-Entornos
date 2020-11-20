3662515561e0f83179aa3b366528922ece7cf2d2
dam <dam@172.26.81.254>
2020-11-20 09:37:43 +0100

    1ª Version RAMA MASTER

---
+++ src/ejercicios/repetitivos/pkg2/EjerciciosRepetitivos2.java
@@ -0,0 +1,32 @@
+/*
+ Escribe un programa que visualice la suma de los 50 
+primeros números impares.
+ */
+package ejercicios.repetitivos.pkg2;
+
+/**
+ *
+ * @author Iván Martínez Albalá
+ */
+public class EjerciciosRepetitivos2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        short numero;
+        int suma;
+        short contador;
+        numero = 1;
+        suma = 0;
+        contador = 0;
+
+        while (contador != 50) {
+            suma = suma + numero;
+            contador = (short) (contador + 1);
+            numero = (short) (numero + 2);
+        }
+        System.out.println("La suma de los primeros "
+                + "50 numeros impares es: " + suma);
+    }
+}
